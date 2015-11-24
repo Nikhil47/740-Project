438a7787993822fb04b9b2934e8965b52f42b6db
Nikhil Singhal <nikhil.singhal90@gmail.com>
2015-11-24 08:46:42 -0500

    Function to compute polynomial fit for given partition

---
+++ calculatePolyFits.R
@@ -0,0 +1,11 @@
+calculatePolyFits <- function(c9, degree = 3){
+    poly.fits = matrix(nrow = 12000, ncol = degree)
+    for(i in 1:1000){
+        # Divide unique ids in 12 parts
+        ids <- sample(x = unique(c9$Person_ID, replace = F))
+        parts <- split(ids, ceiling(seq_along(ids)/2977))
+        
+        poly.fits <- givePBetas(c9, parts, poly.fits, i, degree)
+    }
+    poly.fits
+}
