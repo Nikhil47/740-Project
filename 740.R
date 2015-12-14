57bd5273836025ac86c0af4adb2670310deb4144
Nikhil Singhal <nikhil.singhal90@gmail.com>
2015-12-14 18:48:31 -0500

    Fixed singularity error, Added convergence condition, Fixed plot code to some extent

--- 740.R
+++ 740.R
@@ -16,12 +16,13 @@ c9 <- c9[Person_ID %in% serious.patients]
 
 biWeekDiff <- function(d1, d2){
     # Calculate months from an origin data
-    monthsFromOrigin <- function(d){
-        posixData <- as.POSIXlt(d)
-        posixData$year * 12 + posixData$mon
-    }
-    
-    (monthsFromOrigin(d1) - monthsFromOrigin(d2))
+#     monthsFromOrigin <- function(d){
+#         posixData <- as.POSIXlt(d)
+#         posixData$year * 12 + posixData$mon
+#     }
+#     
+#     (monthsFromOrigin(d1) - monthsFromOrigin(d2))
+    difftime(d1, d2, units = "weeks")/52
 }
 
 # result_date = Date - Date[1] <in months>, to make all records start from month 0
@@ -51,9 +52,11 @@ alpha <- 5
 # Calculated tonnes of Beta values from the data
 poly.fits <- poly.fits.cacheFunctions$getCache()
 # Computing the covariance matrix for the poly.fits
-betas.covariance.matrix <- cov(poly.fits)
+# betas.covariance.matrix <- cov(poly.fits)
+betas.covariance.matrix <- diag(x = 10^2, nrow = degree + 1, ncol = degree + 1)
 # Calculating the means of the Betas
-betas.mean <- apply(poly.fits, 2, mean)
+# betas.mean <- apply(poly.fits, 2, mean)
+beats.mean <- rep(0, degree+1)
 # Using a Multivariate normal distribution to get the intial values of the betas
 betas <- mvrnorm(n = g, betas.mean, betas.covariance.matrix)
 
@@ -66,20 +69,22 @@ values1 <- list()
 values2 <- list()
 expectation.probabilities <- matrix(nrow = length(person_id), ncol = g)
 
-for(loop in 1:3){
+while(convergenceCondition > 0.001){
     # Expectation Step    
     for(i in 1:length(unique(c9$Person_ID))){
-         Xs <- c9[Person_ID == person_id[i], eGFR]
+         Xs <- c9[Person_ID == person_id[i], result_date_months]
 #          phiMat <- data.table(X1 = Xs)
 #          phiMat <- phiMat[, `:=`(X2 = Xs^2, X3 = Xs^3, X4 = Xs^4)]
          phiMat <- data.table(X0 = rep(1, length(Xs)))
          phiMat <- phiMat[, `:=`(X1 = Xs, X2 = Xs^2, X3 = Xs^3, X4 = Xs^4)]
          expectation.mean <- as.matrix(phiMat) %*% t(betas)
-         s <- 37 * diag(length(Xs))
+         s <- 1 * diag(length(Xs))
 #        normal.prob <- rep(1, times = g)
          normal.prob <- numeric(length = g)
+         egfrs <- c9[Person_ID == person_id[i], eGFR]
          for(j in 1:g){
-            normal.prob[j] <- dmvnorm(x = Xs, mean = as.vector(expectation.mean[, j]), sigma = s, log = T)
+            normal.prob[j] <- dmvnorm(x = egfrs,
+                                      mean = as.vector(expectation.mean[, j]), sigma = s, log = T)
          }
          
          normalizing.constant <- sum(normal.prob)
@@ -91,12 +96,12 @@ for(loop in 1:3){
          
          # Values for the Maximization step
          values1[[i]] <- t(as.matrix(phiMat)) %*% solve(s) %*% as.matrix(phiMat)
-         values2[[i]] <- t(as.matrix(phiMat)) %*% solve(s) %*% as.matrix(Xs)
+         values2[[i]] <- t(as.matrix(phiMat)) %*% solve(s) %*% as.matrix(egfrs)
     }
     print("Expectation Step Completed")
     
     new.pis <- vector(mode = "numeric", length = g)
-    new.betas <- matrix(nrow = g, ncol = degree)
+    new.betas <- matrix(nrow = g, ncol = degree+1)
 #     sum.value1 <- matrix(data = 0, nrow = degree, ncol = degree)
 #     sum.value2 <- matrix(data = 0, nrow = degree, ncol = 1)
     
@@ -122,6 +127,8 @@ for(loop in 1:3){
         
         Bracket1[[2]] <- add(temp.list1)
         Bracket2[[2]] <- add(temp.list2)
+        diagMat <- 0.1 * diag(nrow = degree + 1, ncol = degree + 1)
+        #new.betas[j, ] <- solve(add(Bracket1) + diagMat) %*% (add(Bracket2))
         new.betas[j, ] <- solve(add(Bracket1)) %*% (add(Bracket2))
     }
     
@@ -140,7 +147,8 @@ for(loop in 1:3){
     # New values for the next iteration
     betas <- new.betas
     betas.mean <- apply(betas, 2, mean)
-    betas.covariance.inverse <- cov(betas)
+    betas.covariance.matrix <- cov(betas)
+    convergenceCondition <- sum(abs(new.betas - betas)/betas)
     multinomial.probs <- new.pis
     
     # Generate Plot for Beta values
@@ -151,22 +159,24 @@ for(loop in 1:3){
         j <- j + 1
     }
     # 60 : months for 5 years
-    xvalues <- data.frame(x = seq(from = 1, to = 60))
-    betas.plot <- ggplot()
+    gcolors <- c("red", "blue", "black", "green", "purple", "cyan", "yellow", "brown", "orange",
+                 "orchid", "navy")
+    xvalues <- data.frame(x = seq(from = 1, to = 7))
+    betas.plot <- ggplot(data = xvalues, aes(x = x))
     plines <- function(x, row){ 
         eq <- 0
-        for(i in 0:degree)
+        for(i in 1:degree+1)
             eq <- eq + betas[row, i] * (x^i)
         return(eq)
     }
-    r <- 0
+    
     for(i in 1:g){
-        r <- r + 1
         betas.plot <- betas.plot + 
-            stat_function(fun = plines, args = list(row = r), data = xvalues, 
-                          aes(color = paste("line", r, sep = " ")))
+            stat_function(fun = plines, args = list(row = i),
+                          aes(colour = paste("line", i, sep = " ")))
     }
-    betas.plot <- betas.plot + scale_color_brewer("Groups", type = "seq", palette = 1)
+    betas.plot <- betas.plot + 
+                    scale_color_manual("Groups", values = gcolors)
     print(betas.plot)
     # the print statement can be modified to save the plot to disk
 }
@@ -181,7 +191,7 @@ for(i in 1:g){
     
     for(j in head(unique(groupset$Person_ID))){
         group.plot <- group.plot + 
-            stat_function(fun = plines, data = groupset[j, eGFR], args = list(row = i))
+            stat_function(fun = plines, data = as.data.frame(groupset[j, eGFR]), args = list(row = i))
     }
     ggsave(filename = paste("Group", i, sep = " "), path = dest)
 }
