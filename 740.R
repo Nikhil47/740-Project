c11e7fb0dbb3213f6d149f10c55a6e51244d7d95
Nikhil Singhal <nikhil.singhal90@gmail.com>
2015-12-13 20:29:27 -0500

    Added code for plotting graphs and some more changes to other files

--- 740.R
+++ 740.R
@@ -2,28 +2,30 @@ library(data.table)
 library(MASS)
 library(gtools)
 library(mvtnorm)
+library(reshape2)
 
 c9 <- as.data.table(read.table(file = "creatinine.csv", sep = ",", header = T))
 # Coercing the dates to the correct format
 c9[, result_date := as.Date(result_date, origin = "1900-01-01")]
+c9[, group := -1]
 
 # Choosing patients having more than 5 test results
 serious.patients <- c9[, .N >= 5, by = Person_ID][V1 == T, Person_ID]
 # Removing un-serious patients from the table
 c9 <- c9[Person_ID %in% serious.patients]
 
-monthsDiff <- function(d1, d2){
+biWeekDiff <- function(d1, d2){
     # Calculate months from an origin data
     monthsFromOrigin <- function(d){
         posixData <- as.POSIXlt(d)
         posixData$year * 12 + posixData$mon
     }
     
-    monthsFromOrigin(d1) - monthsFromOrigin(d2)
+    (monthsFromOrigin(d1) - monthsFromOrigin(d2))
 }
 
 # result_date = Date - Date[1] <in months>, to make all records start from month 0
-c9 <- c9[, result_date_months := as.numeric(monthsDiff(result_date, result_date[1])),
+c9 <- c9[, result_date_months := as.numeric(biWeekDiff(result_date, result_date[1])),
              by = Person_ID]
 print("Cleaned Data Set Loaded")
 ###############################################################################################
@@ -43,6 +45,9 @@ if(!file.exists("~/740 Project/polyFits.Rda")){
 }
 print("PolyFits Loaded")
 
+g <- 12
+alpha <- 2
+
 # Calculated tonnes of Beta values from the data
 poly.fits <- poly.fits.cacheFunctions$getCache()
 # Computing the covariance matrix for the poly.fits
@@ -50,29 +55,29 @@ betas.covariance.matrix <- cov(poly.fits)
 # Calculating the means of the Betas
 betas.mean <- apply(poly.fits, 2, mean)
 # Using a Multivariate normal distribution to get the intial values of the betas
-betas <- mvrnorm(n = 12, betas.mean, betas.covariance.matrix)
+betas <- mvrnorm(n = g, betas.mean, betas.covariance.matrix)
 
-multinomial.pi <- as.vector(rdirichlet(n = 1, alpha = rep(2, 12)))
+multinomial.pi <- as.vector(rdirichlet(n = 1, alpha = rep(2, g)))
 
 multinomial.probs <- as.vector(rmultinom(n = 1, size = length(unique(c9$Person_ID)), prob = multinomial.pi))
 
 person_id <- unique(c9$Person_ID)
 values1 <- list()
 values2 <- list()
-expectation.probabilities <- matrix(nrow = length(person_id), ncol = 12)
+expectation.probabilities <- matrix(nrow = length(person_id), ncol = g)
 
-for(loop in 1:2){
+for(loop in 1:3){
     # Expectation Step    
     for(i in 1:length(unique(c9$Person_ID))){
          Xs <- c9[Person_ID == person_id[i], result_date_months]
          phiMat <- data.table(X1 = Xs)
-         phiMat <- phiMat[, `:=`(X2 = X1^2, X3 = X1^3, X4 = X1^4)]
+         phiMat <- phiMat[, `:=`(X2 = Xs^2, X3 = Xs^3, X4 = Xs^4)]
          expectation.mean <- as.matrix(phiMat) %*% t(betas)
-         s <- ((0.1)) * diag(length(Xs))
-         normal.prob <- rep(1, times = 12)
-#         normal.prob <- numeric(length = 12)
-#          for(j in 1:12){
-#             normal.prob[j] <- pmvnorm(lower = -Inf, upper = Inf, mean = expectation.mean[, j], sigma = s)[1]
+         s <- 37 * diag(length(Xs))
+        normal.prob <- rep(1, times = g)
+#          normal.prob <- numeric(length = g)
+#          for(j in 1:g){
+#             normal.prob[j] <- dmvnorm(x = Xs, mean = expectation.mean[, j], sigma = s)
 #          }
          
          expectation.probabilities[i, ] <- normal.prob * multinomial.probs
@@ -85,9 +90,8 @@ for(loop in 1:2){
     }
     print("Expectation Step Completed")
     
-    alpha <- 2
-    new.pis <- vector(mode = "numeric", length = 12L)
-    new.betas <- matrix(nrow = 12, ncol = degree)
+    new.pis <- vector(mode = "numeric", length = g)
+    new.betas <- matrix(nrow = g, ncol = degree)
 #     sum.value1 <- matrix(data = 0, nrow = degree, ncol = degree)
 #     sum.value2 <- matrix(data = 0, nrow = degree, ncol = 1)
     
@@ -103,7 +107,7 @@ for(loop in 1:2){
     Bracket2 <- list()
     Bracket2[[1]] <- betas.covariance.inverse %*% betas.mean
     
-    for(j in 1:12){
+    for(j in 1:g){
         temp.list1 <- list()
         temp.list2 <- list()
         for(i in 1:length(unique(c9$Person_ID))){
@@ -117,7 +121,7 @@ for(loop in 1:2){
     }
     
     # Calculating Pis
-    for(i in 1:12){
+    for(i in 1:g){
         new.pis[i] <- sum(expectation.probabilities[, i]) + alpha - 1
     }
     normalizing.constant <- sum(new.pis)
@@ -133,4 +137,45 @@ for(loop in 1:2){
     betas.mean <- apply(betas, 2, mean)
     betas.covariance.inverse <- cov(betas)
     multinomial.probs <- new.pis
+    
+    # Generate Plot for Beta values
+    j <- 1
+    for(i in unique(c9$Person_ID)){
+        maximum <- max(expectation.probabilities[j, ])
+        c9[person_id == i, group := which(maximum == expectation.probabilities[j, ])]
+        j <- j + 1
+    }
+    
+    xvalues <- data.frame(x = seq(from = 1, to = 130))
+    betas.plot <- ggplot(data = xvalues, aes(x))
+    plines <- function(x, row){ 
+        eq <- 0
+        for(i in 0:degree)
+            eq <- eq + betas[row, i] * (x^i)
+        return(eq)
+    }
+    r <- 0
+    for(i in 1:g){
+        r <- r + 1
+        betas.plot <- betas.plot + 
+            stat_function(fun = plines, args = list(row = r), aes(colour = paste("line", r, sep = " ")))
+    }
+    betas.plot <- betas.plot + scale_color_brewer("Groups", type = "seq", palette = 1)
+    print(betas.plot)
+    # the print statement can be modified to save the plot to disk
+}
+
+dest <- paste0("~/", Sys.time())
+dir.create(path = dest)
+print("Folder Created")
+for(i in 1:g){
+    
+    groupset <- c9[, .SD[group == i], by = group]
+    group.plot <- ggplot()
+    
+    for(j in head(unique(groupset$Person_ID))){
+        group.plot <- group.plot + 
+            stat_function(fun = plines, data = groupset[j, eGFR], args = list(row = i))
+    }
+    ggsave(filename = paste("Group", i, sep = " "), path = dest)
 }
--- calculatePolyFits.R
+++ calculatePolyFits.R
@@ -1,8 +1,8 @@
-calculatePolyFits <- function(c9, degree = 3){
+calculatePolyFits <- function(c9, degree = 3, g){
     poly.fits = matrix(nrow = 12000, ncol = degree)
     for(i in 1:1000){
         # Divide unique ids in 12 parts
-        ids <- sample(x = unique(c9$Person_ID, replace = F))
+        ids <- sample(x = unique(c9$Person_ID), replace = F)
         parts <- split(ids, ceiling(seq_along(ids)/2977))
         
         poly.fits <- givePBetas(c9, parts, poly.fits, i, degree)
